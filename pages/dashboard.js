c2d1fe2e0c63e20119718eeb600939388ce3eca6
gkwmedia <tgusewelle@gkwmedia.com>
2022-04-29 07:46:04 -0500

    moved to one loading screen

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -13,6 +13,7 @@ import { CircularProgress } from '@mui/material';
 import WeightForm from "./components/forms/weight-form";
 import Modal from "./components/modal";
 import Alert from "./components/alert"
+import LoadingScreen from "./components/loading-screen";
 
 
 
@@ -22,10 +23,7 @@ ChartJS.register(LineElement, PointElement, LinearScale, Title, CategoryScale, T
 const Dashboard = () => {
     const router = useRouter();
 
-    const [isLoadingForm, setisLoadingForm] = useState(true);
-    const [isLoadingChart, setIsLoadingChart] = useState(true);
-    const [isLoadingWeight, setIsLoadingWeight] = useState(true);
-    const [isLoadingGoal, setIsLoadingGoal] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
     const [isOpen, setIsOpen] = useState(false);
     const [showAlert, setShowAlert] = useState(false);
     
@@ -90,8 +88,9 @@ const Dashboard = () => {
 
         setLabels(labels);
         setWeights(userWeights);
-        setIsLoadingChart(false);
-        setIsLoadingWeight(false);
+
+        setIsLoading(false);
+
     }
 
     const getPostedToday = async (post) => {
@@ -99,10 +98,8 @@ const Dashboard = () => {
         const todayDate = new Date();
        if (postDate.getDate() === todayDate.getDate() && postDate.getMonth() === todayDate.getMonth() && postDate.getFullYear() === todayDate.getFullYear()) {
         setPostedToday(true);
-        setisLoadingForm(false);
        } else {
         setPostedToday(false);
-        setisLoadingForm(false);
        }  
        
     }
@@ -126,7 +123,7 @@ const Dashboard = () => {
         const userPercentage = parseInt(100 - ((profile.target_weight / lastWeight) * 100));
         setLossPercentage(userPercentage);
         setCurrentWeight(lastWeight);        
-        setIsLoadingGoal(false);
+
     }
     
 
@@ -199,6 +196,8 @@ const Dashboard = () => {
 
     return (
         <div className="wrapper">
+
+            {isLoading && <LoadingScreen/>}
             
             <AnimatePresence>       
                 {showAlert && 
@@ -208,25 +207,16 @@ const Dashboard = () => {
                 </Alert>
                 }
                 </AnimatePresence>     
-               
-           
 
-            
             <div className="py-4 mx-auto">
   
                 {profile && <h1 className="text-4xl text-center text-white">{weights.length > 1 ? "Welcome back" : "Welcome to Weight Tracker"} {profile.first_name}!</h1>}
                 <div className="grid grid-cols-2 gap-4 justify-center mt-6 sm:flex ">
                     <div className="min-w-[10rem] bg-stone-900 flex items-center p-2 rounded-xl justify-center text-green-500">
-                        { isLoadingWeight
-                            ? <CircularProgress color={"inherit"}/>
-                            : <h3 className="text-white text-xl">Weight: {currentWeight != undefined && currentWeight.toString()}</h3>
-                        }
+                        <h3 className="text-white text-xl">Weight: {currentWeight != undefined && currentWeight.toString()}</h3>
                     </div>
                     <div className={` min-w-[10rem] flex items-center justify-center py-2 px-4 rounded-xl bg-stone-900 text-green-500`}>
-                        { isLoadingGoal
-                            ? <CircularProgress color="inherit"/>
-                            :<>
-                                <h3 className="text-xl text-white">Goal: {targetWeight != 0 && targetWeight}</h3>
+                        <h3 className="text-xl text-white">Goal: {targetWeight != 0 && targetWeight}</h3>
                         
                             {/* {lossPercentage != undefined && <div className="relative w-min h-[50px] ml-auto text-green-500">
                                 <CircularProgress
@@ -239,9 +229,8 @@ const Dashboard = () => {
                                     className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-sm">
                                     {lossPercentage.toString()}%
                                 </p>
-                            </div>} */}
-                        </>
-                        }
+                            </div>} */}  
+                        
                     </div>
                     <button 
                       className="col-span-2 text-lg text-white font-semibold bg-green-500 py-2 px-6 rounded-md focus:outline-none "
@@ -266,16 +255,14 @@ const Dashboard = () => {
             </Modal>
             
             <div className="min-w-screen max-w-full flex flex-col items-center mt-1 lg:flex-row lg:w-full">
-                {isLoadingChart 
-                 ? <div className="w-full flex items-center justify-center sm:h-64 text-green-500">
-                <CircularProgress color="inherit"></CircularProgress>
-                </div>
-                : <div className="w-full w-min-[16rem] sm:mt-4">
-                {weights.length > 0 
-                    ? <h2 className="text-white text-center">Your weight over the past {numberOfWeightDays} {weights.length > 1 ? <span>days</span> : <span>day</span>
-                    }</h2>
-                    : <h2 className="text-white text-center">Please submit your weight to begin tracking</h2>
-                }
+                 <div className="w-full w-min-[16rem] sm:mt-4">
+                    <h2 
+                        className="text-white text-center">
+                        Your weight over the past {numberOfWeightDays} 
+                        {weights.length > 1 ? <span>days</span> : <span>day</span>}
+                    </h2>
+                    
+                
                 <div className="w-full">
                 <Line 
                     data={data} 
@@ -314,7 +301,7 @@ const Dashboard = () => {
                 </Line>
                 </div>
                 </div>
-                } 
+                
                 
             </div>
             {/* These buttons are for testing purposes */}
