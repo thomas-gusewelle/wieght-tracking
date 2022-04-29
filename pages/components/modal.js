51ebafa4ace16754f815d069c91bd0790d4658a8
gkwmedia <tgusewelle@gkwmedia.com>
2022-04-29 07:46:27 -0500

    fixed modal sizing

--- pages/components/modal.js
+++ pages/components/modal.js
@@ -4,9 +4,10 @@ const Modal = ({open, onClose, children}) => {
         
         <>
         <div 
-        onClick={onClose}
-        className="fixed top-0 left-0 right-0 bottom-0 bg-stone-700/75 "></div>
-        <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50 bg-stone-900 p-8 rounded-3xl">
+            onClick={onClose}
+            className="fixed top-0 left-0 right-0 bottom-0 bg-stone-700/75 ">
+        </div>
+        <div className="fixed min-w-fit top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50 bg-stone-900 p-8 rounded-3xl">
 
             {children}
 
