17b25af8701b9badf67504f06810e7fba1e43ab3
gkwmedia <tgusewelle@gkwmedia.com>
2022-04-29 17:17:29 -0500

    fixed chart visibility during loading

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -192,126 +192,130 @@ const Dashboard = () => {
       };
 
 
+        return (
+            <>
+            {isLoading ? <div className="wrapper"><LoadingScreen></LoadingScreen></div>
+             :<div className="wrapper">
 
-    return (
-        <div className="wrapper">
-
-            {isLoading && <LoadingScreen/>}
-            
-            <AnimatePresence>       
-                {showAlert && 
-                <Alert 
-                setShowAlert={setShowAlert}>
-                    <p className="text-center">You have already logged your weight today. Come back tomorrow to keep tracking!</p>
-                </Alert>
-                }
-                </AnimatePresence>     
-
-            <div className="py-4 mx-auto">
-  
-                {profile && <h1 className="text-4xl text-center text-white">{weights.length > 1 ? "Welcome back" : "Welcome to Weight Tracker"} {profile.first_name}!</h1>}
-                <div className="grid grid-cols-2 gap-4 justify-center mt-6 sm:flex ">
-                    <div className="min-w-[10rem] bg-stone-900 flex items-center p-2 rounded-xl justify-center text-green-500">
-                        <h3 className="text-white text-xl">Weight: {currentWeight != undefined && currentWeight.toString()}</h3>
+                <AnimatePresence>       
+                    {showAlert && 
+                    <Alert 
+                    setShowAlert={setShowAlert}>
+                        <p className="text-center">You have already logged your weight today. Come back tomorrow to keep tracking!</p>
+                    </Alert>
+                    }
+                    </AnimatePresence>     
+    
+                <div className="py-4 mx-auto">
+      
+                    {profile && <h1 className="text-4xl text-center text-white">{weights.length > 1 ? "Welcome back" : "Welcome to Weight Tracker"} {profile.first_name}!</h1>}
+                    <div className="grid grid-cols-2 gap-4 justify-center mt-6 sm:flex ">
+                        <div className="min-w-[10rem] bg-stone-900 flex items-center p-2 rounded-xl justify-center text-green-500">
+                            <h3 className="text-white text-xl">Weight: {currentWeight != undefined && currentWeight.toString()}</h3>
+                        </div>
+                        <div className={` min-w-[10rem] flex items-center justify-center py-2 px-4 rounded-xl bg-stone-900 text-green-500`}>
+                            <h3 className="text-xl text-white">Goal: {targetWeight != 0 && targetWeight}</h3>
+                            
+                                {/* {lossPercentage != undefined && <div className="relative w-min h-[50px] ml-auto text-green-500">
+                                    <CircularProgress
+                                        variant="determinate" 
+                                        size={50} 
+                                        value={lossPercentage}
+                                        color={"inherit"} 
+                                    />
+                                    <p 
+                                        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-sm">
+                                        {lossPercentage.toString()}%
+                                    </p>
+                                </div>} */}  
+                            
+                        </div>
+                        <button 
+                          className="col-span-2 text-lg text-white font-semibold bg-green-500 py-2 px-6 rounded-md focus:outline-none "
+                            onClick={handleModalOpen}
+                            > Log Your Weight
+                        </button>
                     </div>
-                    <div className={` min-w-[10rem] flex items-center justify-center py-2 px-4 rounded-xl bg-stone-900 text-green-500`}>
-                        <h3 className="text-xl text-white">Goal: {targetWeight != 0 && targetWeight}</h3>
-                        
-                            {/* {lossPercentage != undefined && <div className="relative w-min h-[50px] ml-auto text-green-500">
-                                <CircularProgress
-                                    variant="determinate" 
-                                    size={50} 
-                                    value={lossPercentage}
-                                    color={"inherit"} 
-                                />
-                                <p 
-                                    className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-sm">
-                                    {lossPercentage.toString()}%
-                                </p>
-                            </div>} */}  
-                        
-                    </div>
-                    <button 
-                      className="col-span-2 text-lg text-white font-semibold bg-green-500 py-2 px-6 rounded-md focus:outline-none "
-                        onClick={handleModalOpen}
-                        > Log Your Weight
-                    </button>
-                </div>
-
-                
-            </div>
-            <Modal open={isOpen} onClose={onModalClose}>
-             <WeightForm 
-             onClose={onModalClose} 
-             setPostedToday={setPostedToday}
-             getUserWeights={getUserWeights}
-             weights={weights}
-             setWeights={setWeights}
-             >
-
-             </WeightForm>
-             
-            </Modal>
-            
-            <div className="min-w-screen max-w-full flex flex-col items-center mt-1 lg:flex-row lg:w-full">
-                 <div className="w-full w-min-[16rem] sm:mt-4">
-                    <h2 
-                        className="text-white text-center">
-                        Your weight over the past {numberOfWeightDays} 
-                        {weights.length > 1 ? <span>days</span> : <span>day</span>}
-                    </h2>
+    
                     
+                </div>
+                <Modal open={isOpen} onClose={onModalClose}>
+                 <WeightForm 
+                 onClose={onModalClose} 
+                 setPostedToday={setPostedToday}
+                 getUserWeights={getUserWeights}
+                 weights={weights}
+                 setWeights={setWeights}
+                 >
+    
+                 </WeightForm>
+                 
+                </Modal>
                 
-                <div className="w-full">
-                <Line 
-                    data={data} 
-                    width={400} 
-                    height={400} 
-                    options={
-                        {
-                        responsive: true, 
-                        maintainAspectRatio: false,
-                        scales: {
-                            y: {
-                                
+                <div className="min-w-screen max-w-full flex flex-col items-center mt-1 lg:flex-row lg:w-full">
+                     <div className="w-full w-min-[16rem] sm:mt-4">
+                        <h2 
+                            className="text-white text-center">
+                            Your weight over the past {numberOfWeightDays} 
+                            {weights.length > 1 ? <span>days</span> : <span>day</span>}
+                        </h2>
+                        
+                    
+                    <div className="w-full">
+                    <Line 
+                        data={data} 
+                        width={400} 
+                        height={400} 
+                        options={
+                            {
+                            responsive: true, 
+                            maintainAspectRatio: false,
+                            scales: {
+                                y: {
+                                    
+                                }
+                            },
+                            plugins: {
+                                // annotation: {
+                                //     annotations: [
+                                //         {
+                                //             type: "line",
+                                //             yMax:targetWeight,
+                                //             yMin:targetWeight, 
+                                //             borderColor: "red",
+                                //             label: {
+                                //                 enabled: true,
+                                //                 content: "Target Weight"
+                                //             }
+                                                
+                                //         }
+                                //     ]
+                                // }
+                            },
+    
                             }
-                        },
-                        plugins: {
-                            // annotation: {
-                            //     annotations: [
-                            //         {
-                            //             type: "line",
-                            //             yMax:targetWeight,
-                            //             yMin:targetWeight, 
-                            //             borderColor: "red",
-                            //             label: {
-                            //                 enabled: true,
-                            //                 content: "Target Weight"
-                            //             }
-                                            
-                            //         }
-                            //     ]
-                            // }
-                        },
-
-                        }
-                        }>
-
-                </Line>
+                            }>
+    
+                    </Line>
+                    </div>
+                    </div>
+                    
+                    
                 </div>
-                </div>
-                
-                
+                {/* These buttons are for testing purposes */}
+                {/* <button className="bg-white p-4"
+                onClick={() => setPostedToday(!postedToday)}
+                >change log state</button> */}
+                {/* <button className="bg-white p-4"
+                onClick={() => setShowAlert(!postedToday)}
+                >change log state</button> */}
             </div>
-            {/* These buttons are for testing purposes */}
-            {/* <button className="bg-white p-4"
-            onClick={() => setPostedToday(!postedToday)}
-            >change log state</button> */}
-            {/* <button className="bg-white p-4"
-            onClick={() => setShowAlert(!postedToday)}
-            >change log state</button> */}
-        </div>
-    )
-}
+            }
+            </>
+        )
+    }
+    
+
+
 
 export default Dashboard 
