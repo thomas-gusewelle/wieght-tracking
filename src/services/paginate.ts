5d042802bae2b4345a86904622364cba87e18bd4
Thomas <tgusewelle@gkwmedia.com>
2022-06-01 15:19:06 -0500

    added pagination service

---
+++ src/services/paginate.ts
@@ -0,0 +1,20 @@
+function paginate(items: [], currentPage: number, perPageItems: number) {
+  let page = currentPage || 1;
+  let perPage = perPageItems || 30;
+  let offset = (page - 1) * perPage;
+
+  let paginatedItems = items.slice(offset).slice(0, perPageItems);
+  let totalPages = Math.ceil(items.length / perPage);
+
+  return {
+    page: page,
+    per_page: perPage,
+    prev_page: page - 1 ? page - 1 : null,
+    next_page: totalPages > page ? page + 1 : null,
+    total: items.length,
+    total_pages: totalPages,
+    data: paginatedItems,
+  };
+}
+
+export default paginate;
