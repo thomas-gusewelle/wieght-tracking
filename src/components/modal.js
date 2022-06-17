8021c4dbbb42ba0564b68b0171f1c5a28d1ca7eb
gkwmedia <tgusewelle@gkwmedia.com>
2022-06-17 14:44:17 -0500

    added alert to modal with optional props

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,10 +1,23 @@
-const Modal = ({ open, onClose, children }) => {
+import Alert from "./alert";
+
+const Modal = ({
+  open,
+  onClose,
+  children,
+  showAlert = false,
+  setShowAlert = {},
+  alertMessage = "",
+}) => {
   if (!open) return;
   return (
     <>
       <div
         onClick={onClose}
-        className='fixed top-0 left-0 right-0 bottom-0 bg-stone-700/75 '></div>
+        className='fixed top-0 left-0 right-0 bottom-0 bg-stone-700/75 '>
+        <Alert showAlert={showAlert} setShowAlert={setShowAlert}>
+          <p className='text-center'>{alertMessage}</p>
+        </Alert>
+      </div>
       <div className='fixed mx-2 min-w-fit top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50 bg-stone-900 p-8 rounded-3xl'>
         {children}
       </div>
